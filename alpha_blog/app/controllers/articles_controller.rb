eeb72b9019a2adedd375f81e51d9e1281df690b2
JWizerd <jwodke@outlook.com>
2016-06-24 16:51:45 -0600

    created show all listings view, set up page navigation

--- alpha_blog/app/controllers/articles_controller.rb
+++ alpha_blog/app/controllers/articles_controller.rb
@@ -1,6 +1,24 @@
 class ArticlesController < ApplicationController
+  def index 
+    @articles = Article.all
+  end
+
   def new 
-    @article = Article.new
+    @article = Article.new #creates new article from articles/new view
+  end
+
+  def edit
+    @article = Article.find(params[:id]) #find the id of a created article
+  end
+
+  def update #notes from here are valid for create method as well.
+    @article = Article.find(params[:id]) 
+    if @article.update(article_params) # if article params (line 40) are true run code
+      flash[:notice] = "Article was successfully edited" #this is a rails method that shows an notification if errors are present in submission
+      redirect_to article_path(@article) # after 
+    else 
+      render :edit # if article params (line 40) is not true reload edit page (errors will occur from edit.html.erb)
+    end
   end
   
   def create
